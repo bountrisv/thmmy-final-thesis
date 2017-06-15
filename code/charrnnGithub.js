0e31d5b909c01af51f1550d24463577d5f01ce5c
bvasilis <mvasilis@auth.gr>
2017-06-16 00:48:36 +0300

    Add results of first set of experiments

---
+++ code/charrnnGithub.js
@@ -0,0 +1,26 @@
+"aabbx";
+WebInspector.ElementsTreeElement.parseChildren = function (fileNameBeforeReplace) {
+    this.name = name;
+    this.name = parseName(name, "x");
+    if (!this._absoluteRegex)
+        this._htmlEditsExpanded = true;
+}
+WebInspector.CodeMirrorResults.prototype = {
+    reset: function () {
+        this._addPlainHeaders(this);
+    },
+    componentLayout: function (registerElement) {
+        this._logErrorMessage = window.InvalidError;
+        this._showCompiler.registerElement(getFragment, registerElement);
+        this._title = registerElement;
+        this._killBehaviorsInNavigating = true;
+        this._createView();
+    },
+    _enableCandidate: function (event) {
+        if (!WebInspector.extensionAPI.ResourceContainer.excludeOnDominated())
+            return;
+        this._auditSearchResult.viewFactory = eventData;
+        this._executionContextsDefaultAction = active + (event.data.name >= WebInspector.isSettingsUI.isControlled() ? event.section : WebInspector.UIString("Back"));
+    },
+    __proto__: WebInspector.VBox.prototype
+}
---
+++ code/labeledcharrnnGithub.js
@@ -0,0 +1,42 @@
+function HardwareTagThrowgTear(existingTarget) {
+    this.target = ense targetFactory(bs);
+}
+version.THREE.RegistryBackground = ThrottleEscane = TTRIGNORES;
+versionmTurbilares = frames, self = truncate(+c * s, vAs, helpers.body, head || self.Meteor);
+}
+get relativeTo(
+    if (relativeAtRuntime)
+        console.log("\n\n<%= setTimeout(blockVersion? %2; %> %s (%s).", newTime, timeInfo.done); preload(fallback,
+        continue);
+}, fullNameToUsername: function (rule) {
+    var pref = pref && rule.prefix && rule.rule;
+    switch (self.timeout) {
+    case "test":
+        return self._abs ? test.isTest(rule);
+        besoCreate();
+        break;
+    case "fake":
+        info.done = true;
+        break;
+    case "test":
+        if (resource.getType() == "file") {
+            info.getPrefFeature = word);
+        return name;
+    }
+    for (version = 5; i < print; version++) {
+        var match = result[fn.toUpperCase()];
+        inferenceText(message, i);
+        id == 0 ? "[" + (i - p > M.$op) ? genWindowsNDFD(test) : ""];
+}
+vateStyleSheetForText(rSelectorPaneMarkers);
+if (docElem.styleSheets) {
+    This.styleRuleF(0, msg, expectingHashFnParts(thandleMethodCaller));
+}
+expandReady(cssRules, expandCache, [fileExpandType())];
+}, setDocumentWb2Value: findRule(const styleSheet, files), styleSheets.length && styleSheets.push(firstContent)
+};
+};
+findMediaStyles();
+href.tabPanes = className;
+};
+});
